3d67cd01d3693130d4aefa73450d8aaab11e3d49
aringos <32782879+aringos@users.noreply.github.com>
2017-10-14 21:33:10 -0700

    High-level EKF pseudocode and filtering equations

--- triangulateEkf.m
+++ triangulateEkf.m
@@ -1,20 +1,21 @@
 %--------------------------------------------------------------------------
 %
-% triangulationKalman
+% triangulateEkf
 %
 % Austin Smith
 % SIE-554A
 % 10/13/2017
 %
 % Triangulates observed vehicle dynamics based on multiple passive sensor
-% estimates in NED frame, where each sensor location is known.
+% estimates in NED frame, where each sensor location is known. This version
+% is an extended Kalman filter.
 %
 % Inputs:  current filtered state (x0=[x,y,z,vx,vy,vz])
 %          current filtered error (diag(p0)=[xVar,yVar,zVar,vxVar,vyVar,vzVar])
-%          observation vectors (x1=[el,elDot,az,azDot]; ...
-%                               xN=[...])
-%          observation noise (diag(p1)=[elVar,elDotVar,azVar,azDotVar]; ...
-%                             diag(pN)=[...])
+%          observation vectors (z1=[el,elDot,az,azDot]; ...
+%                               z2=[...])
+%          observation noise (diag(r1)=[elVar,elDotVar,azVar,azDotVar]; ...
+%                             diag(r2)=[...])
 %          time vector (time=[tObs_prev,tObs,tEst])
 %              where the times are: prev. observation, current, and desired
 %              filter estimate time
@@ -26,18 +27,40 @@
 %
 %--------------------------------------------------------------------------
 
-function [ xHat,pHat,xBar,pBar ] = triangulationKalman( x0,p0,z,R,time )
-
-% Determine sensor availability (2-3 preferred)
-[numSensors,~] = size(z);
+function [ xHat,pHat,xBar,pBar ] = triangulateEkf( x0,p0,z,r,time )
 
 % Choose best (not near n*pi) gamma, if possible
+% Choose best (not +/-pi/2) elevations, if possible
+% Do this outside the filter!
+
+% Extended Kalman filter
+%
+% Predicted at measurement:
+% xBar=Phi*x0
+% pBar=Phi*p0*Phi'+Q
+% zBar=H*xBar
+% rBar=H*pBar*H'
+%
+% Filtering:
+% y=z-zBar
+% S=r+rBar
+% K=pBar*H'*S^-1
+% xHat=xBar+K*y
+% pHat=(I-K*H)*pBar
+%
+% Extrapolated estimate:
+% ...Same eqns as predicted
+
+% Predicted measurements and states
 % ...
 
-% Determine math and filtering for two best sensors
+% Measurement matrix
 % ...
 
-% Provide extrapolated estimate
+% Filter measurements
+% ...
+
+% Extrapolate estimate
 % ...
 
 end

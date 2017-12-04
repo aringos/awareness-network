586378d3a749c844fbfd0a32b55cdd725d881674
aringos <32782879+aringos@users.noreply.github.com>
2017-12-03 19:27:56 -0700

    Monte Carlo loop functionality
    Still need to develop tools to look at output metrics more efficiently

--- simDriver.m
+++ simDriver.m
@@ -12,6 +12,7 @@
 %
 % Inputs:  Plot toggles
 %          Time vector
+%          Monte Carlo seed vector
 %          Scenario set stored in cell array
 %
 % Outputs: FOMs (cost vs. performance)
@@ -28,18 +29,18 @@ addpath('scenarios');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Driver Setup
 
-plotGeometry            = 1;
+plotGeometry            = 0;
 plotSensorEstimates     = 0;
 plotNetworkPacketDelays = 0;
 plotFusion              = 0;
 plotTrueState           = 0;
 plotEstimateResiduals   = 0;
-plotEstimates           = 1;
+plotEstimates           = 0;
 
 dt         = 0.001;
 tend       = 51.5; %A major block in midtown Tucson is about 600-800m long
                    %tend = 800m/15.6mps
-randomSeed = 0;  
+randomSeeds = 1:2;  
 
 scenarioSet = ...
     { scn_RaspPi1080p_LoRa(dt,tend),...
@@ -55,12 +56,13 @@ scenarioSet = ...
 % Run Scenario Set
 
 numScn = length(scenarioSet);
-scnData = struct('metricEstimate',cell(1,numScn),'x_hist',cell(1,numScn),...
-                 'P_hist',cell(1,numScn),'x',cell(1,numScn));
+numSeeds = length(randomSeeds);
+emptyCell = cell(numScn,numSeeds);
+scnData = struct('metricEstimate',emptyCell,'x_hist',emptyCell,...
+                 'P_hist',emptyCell,'x',emptyCell);
 t = 0:dt:tend;
-rng(randomSeed);
 
-% Scenario loop
+% Scenario Loop
 for scn = 1:numScn
     
     sensors  = scenarioSet{scn}.sensors;
@@ -68,141 +70,145 @@ for scn = 1:numScn
     hardware = scenarioSet{scn}.hardware;
     fusion   = scenarioSet{scn}.fusion;
     accel    = scenarioSet{scn}.vehicleAccel;
-    
-    x_hist = zeros(4,length(t));
-    P_hist = zeros(4,length(t));
 
     %Get target state for all time t
     x = getStateVector(accel,scenarioSet{scn}.vehicleInitD,...
                        scenarioSet{scn}.vehicleInitV,dt);
 
-    %Simulation Loop
-    for k=1:length(t)
-       currState = x(:,k);
+    %Monte Carlo Loop
+    for seed = 1:numSeeds
+        rng(randomSeeds(seed));
+        
+        x_hist = zeros(4,length(t));
+        P_hist = zeros(4,length(t));
+        
+        %Simulation Loop
+        for k=1:length(t)
+           currState = x(:,k);
 
-       %Update sensors and their trackers
-       for s=1:length(sensors)
-           sensors(s) = sensors(s).update(currState,t(k));
-       end
+           %Update sensors and their trackers
+           for s=1:length(sensors)
+               sensors(s) = sensors(s).update(currState,t(k));
+           end
 
-       %Get observations (will be timestamped, not necessarily all aligned)
-       rawObservationList = [];
-       if network.observationsAvailable(t(k))
-          [network, rawObservationList] = ...
-              network.rawObservationListAtTime(sensors, t(k)); 
-       end
+           %Get observations (will be timestamped, not necessarily all aligned)
+           rawObservationList = [];
+           if network.observationsAvailable(t(k))
+              [network, rawObservationList] = ...
+                  network.rawObservationListAtTime(sensors, t(k)); 
+           end
 
-       %Convert rawObservationList into refinedObservationList:
-       %Basically, any necessary triangulation is performed here and we
-       %assume that these will all have full rank (angle,range,rangerate)
-       refinedObservationList = [];
-       for i=1:length(rawObservationList)
-           if rawObservationList(i).observation_H == [1;0;1;1]
-              observation = FusionObservation( ...
-                                rawObservationList(i).observation_z, ...
-                                rawObservationList(i).observation_P, ...
-                                rawObservationList(i).observation_t, ...
-                                rawObservationList(i).sensor_pos);
-              observation.type = 'RADAR';
-              refinedObservationList = [refinedObservationList observation]; 
-           elseif rawObservationList(i).observation_H == [1;1;0;0]
-               for j=i+1:length(rawObservationList)
-                   if rawObservationList(i).observation_H == [1;1;0;0]
-                      refinedObservationList = [refinedObservationList ...
-                          triangulateObservations(rawObservationList(i), ...
-                              rawObservationList(j))];
+           %Convert rawObservationList into refinedObservationList:
+           %Basically, any necessary triangulation is performed here and we
+           %assume that these will all have full rank (angle,range,rangerate)
+           refinedObservationList = [];
+           for i=1:length(rawObservationList)
+               if rawObservationList(i).observation_H == [1;0;1;1]
+                  observation = FusionObservation( ...
+                                    rawObservationList(i).observation_z, ...
+                                    rawObservationList(i).observation_P, ...
+                                    rawObservationList(i).observation_t, ...
+                                    rawObservationList(i).sensor_pos);
+                  observation.type = 'RADAR';
+                  refinedObservationList = [refinedObservationList observation]; 
+               elseif rawObservationList(i).observation_H == [1;1;0;0]
+                   for j=i+1:length(rawObservationList)
+                       if rawObservationList(i).observation_H == [1;1;0;0]
+                          refinedObservationList = [refinedObservationList ...
+                              triangulateObservations(rawObservationList(i), ...
+                                  rawObservationList(j))];
+                       end
                    end
                end
            end
-       end
 
-       %Update Sensor fusion kalman filter
-       for i=1:length(refinedObservationList)
-           fusion = fusion.update(refinedObservationList(i));
-       end
+           %Update Sensor fusion kalman filter
+           for i=1:length(refinedObservationList)
+               fusion = fusion.update(refinedObservationList(i));
+           end
 
-       [x_est,P_est] = fusion.extrapolate(t(k));
-       x_hist(:,k) = x_est;
-       P_hist(:,k) = P_est;
-       
-    end
+           [x_est,P_est] = fusion.extrapolate(t(k));
+           x_hist(:,k) = x_est;
+           P_hist(:,k) = P_est;
 
-    metricEstimate = CostPerformanceEstimate(x, x_hist, sensors, network, hardware);
+        end
 
-    % Store scenario output
-    scnData(scn).metricEstimate = metricEstimate;
-    scnData(scn).x_hist = x_hist;
-    scnData(scn).P_hist = P_hist;
-    scnData(scn).x = x;
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Telemetry Plots
+        metricEstimate = CostPerformanceEstimate(x, x_hist, sensors, network, hardware);
 
-    if plotTrueState
-       figure;
-       subplot(4,1,1);
-       plot(t, x(1,:)); title('EAST POS');
-       subplot(4,1,2);
-       plot(t, x(2,:)); title('NORTH POS');
-       subplot(4,1,3);
-       plot(t, x(3,:)); title('EAST VEL');
-       subplot(4,1,4);
-       plot(t, x(4,:)); title('NORTH VEL');
-    end
+        % Store scenario output
+        scnData(scn,seed).metricEstimate = metricEstimate;
+        scnData(scn,seed).x_hist = x_hist;
+        scnData(scn,seed).P_hist = P_hist;
+        scnData(scn,seed).x = x;
 
-    if plotNetworkPacketDelays
-        network.plotPacketDelays();
-    end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Telemetry Plots
 
-    if plotSensorEstimates
-        for s=1:length(sensors)
-           sensors(s).plotTelemetry(); 
+        if plotTrueState
+           figure;
+           subplot(4,1,1);
+           plot(t, x(1,:)); title('EAST POS');
+           subplot(4,1,2);
+           plot(t, x(2,:)); title('NORTH POS');
+           subplot(4,1,3);
+           plot(t, x(3,:)); title('EAST VEL');
+           subplot(4,1,4);
+           plot(t, x(4,:)); title('NORTH VEL');
+        end
+
+        if plotNetworkPacketDelays
+            network.plotPacketDelays();
+        end
+
+        if plotSensorEstimates
+            for s=1:length(sensors)
+               sensors(s).plotTelemetry(); 
+            end
+        end
+
+        if plotGeometry
+            figure; hold on; grid on; axis equal;
+            title('Scenario Geometry');
+            xlabel('East (m)'); ylabel('North (m)');
+            plot(x(1,:), x(2,:), 'k-', 'LineWidth', 2);
+            for i=1:length(sensors)
+               plot(sensors(i).vertices(1,:), sensors(i).vertices(2,:), ...
+                   'Color', sensors(i).color); 
+            end
+        end
+
+        if plotFusion
+            fusion.plotTelemetry();
+        end
+
+        if plotEstimates
+           figure;
+           subplot(4,1,1); hold on; grid on; title('East Position (m)');
+           plot(t, x(1,:), 'g-', 'LineWidth', 2);
+           plot(t, x_hist(1,:), 'k-');
+           plot(t, x_hist(1,:)+sqrt(P_hist(1,:)), 'r--');
+           plot(t, x_hist(1,:)-sqrt(P_hist(1,:)), 'r--');
+           subplot(4,1,2); hold on; grid on; title('East Velocity (m/s)');
+           plot(t, x(3,:), 'g-', 'LineWidth', 2);
+           plot(t, x_hist(2,:), 'k-');
+           plot(t, x_hist(2,:)+sqrt(P_hist(2,:)), 'r--');
+           plot(t, x_hist(2,:)-sqrt(P_hist(2,:)), 'r--');
+           subplot(4,1,3); hold on; grid on; title('North Position (m)');
+           plot(t, x(2,:), 'g-', 'LineWidth', 2);
+           plot(t, x_hist(3,:), 'k-');
+           plot(t, x_hist(3,:)+sqrt(P_hist(3,:)), 'r--');
+           plot(t, x_hist(3,:)-sqrt(P_hist(3,:)), 'r--');
+           subplot(4,1,4); hold on; grid on; title('North Velocity (m/s)');
+           plot(t, x(4,:), 'g-', 'LineWidth', 2);
+           plot(t, x_hist(4,:), 'k-');
+           plot(t, x_hist(4,:)+sqrt(P_hist(4,:)), 'r--');
+           plot(t, x_hist(4,:)-sqrt(P_hist(4,:)), 'r--');
+           xlabel('Time (s)');
+        end
+
+        if plotEstimateResiduals
+           metricEstimate.plotResiduals(t); 
         end
     end
-
-    if plotGeometry
-        figure; hold on; grid on; axis equal;
-        title('Scenario Geometry');
-        xlabel('East (m)'); ylabel('North (m)');
-        plot(x(1,:), x(2,:), 'k-', 'LineWidth', 2);
-        for i=1:length(sensors)
-           plot(sensors(i).vertices(1,:), sensors(i).vertices(2,:), ...
-               'Color', sensors(i).color); 
-        end
-    end
-
-    if plotFusion
-        fusion.plotTelemetry();
-    end
-
-    if plotEstimates
-       figure;
-       subplot(4,1,1); hold on; grid on; title('East Position (m)');
-       plot(t, x(1,:), 'g-', 'LineWidth', 2);
-       plot(t, x_hist(1,:), 'k-');
-       plot(t, x_hist(1,:)+sqrt(P_hist(1,:)), 'r--');
-       plot(t, x_hist(1,:)-sqrt(P_hist(1,:)), 'r--');
-       subplot(4,1,2); hold on; grid on; title('East Velocity (m/s)');
-       plot(t, x(3,:), 'g-', 'LineWidth', 2);
-       plot(t, x_hist(2,:), 'k-');
-       plot(t, x_hist(2,:)+sqrt(P_hist(2,:)), 'r--');
-       plot(t, x_hist(2,:)-sqrt(P_hist(2,:)), 'r--');
-       subplot(4,1,3); hold on; grid on; title('North Position (m)');
-       plot(t, x(2,:), 'g-', 'LineWidth', 2);
-       plot(t, x_hist(3,:), 'k-');
-       plot(t, x_hist(3,:)+sqrt(P_hist(3,:)), 'r--');
-       plot(t, x_hist(3,:)-sqrt(P_hist(3,:)), 'r--');
-       subplot(4,1,4); hold on; grid on; title('North Velocity (m/s)');
-       plot(t, x(4,:), 'g-', 'LineWidth', 2);
-       plot(t, x_hist(4,:), 'k-');
-       plot(t, x_hist(4,:)+sqrt(P_hist(4,:)), 'r--');
-       plot(t, x_hist(4,:)-sqrt(P_hist(4,:)), 'r--');
-       xlabel('Time (s)');
-    end
-
-    if plotEstimateResiduals
-       metricEstimate.plotResiduals(t); 
-    end
-
 end
